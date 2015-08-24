23b898dcddc569a7a0cc05b286aca65ee9cb2ce0
concuacon <nguyentuan1707@gmail.com>
2015-08-24 17:43:23 +0700

    editing prescription screen

--- client/templates/prescription.js
+++ client/templates/prescription.js
@@ -1,18 +1,20 @@
-
+﻿
 
 
 Template.InsertThuoc.onRendered(function () {
     $('#txtTime').dropdown();
 });
 
-Template.prescriptionTemplate.events({
-    'click [data-action="nhaptenthuoc"]': function () {
-        SemanticModal.generalModal('InsertThuoc', { foo: 'bar' });
-    }
-});
-
+//Bắt sự kiện form nhập thuốc
 Template.InsertThuoc.events({
     'click #btnLuuthuoc': function (event) {
         
+    }
+});
+//bắt sự kiện form nhập tên đơn thuốc
+Template.prescriptionTemplate.events({
+
+    'click [data-action="nhaptenthuoc"]': function () {
+        SemanticModal.generalModal('InsertThuoc', { foo: 'bar' });
     }
 });
