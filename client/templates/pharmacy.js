066e34612eb712e5e1f27f8a5372b029eb4d4ba4
thanhtuanfh <thanhtuanfh@gmail.com>
2015-08-27 09:36:21 +0700

    load maker to maps

--- client/templates/pharmacy.js
+++ client/templates/pharmacy.js
@@ -7,25 +7,30 @@ Template.pharmacy.onCreated(function() {
   self.autorun(function() {
     self.subscribe('searchphama');
   });
-  GoogleMaps.ready('exampleMap', function(map) {
-    // Add a marker to the map once it's ready
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
-    });
-  });
 });
 Template.pharmacy.helpers({
     ShowAll: function() {
       return SearchAllPhano.find({});
     },
     exampleMapOptions: function() {
+      debugger;
+    var listPhano = SearchAllPhano.find().fetch();
+console.log(listPhano);
+      GoogleMaps.ready('exampleMap', function(map) {
+        // Add a marker to the map once it's ready
+        _.each(listPhano,function(phano){
+          var marker = new google.maps.Marker({
+            position: {lat:phano.LAT,lng:phano.LONG},
+            map: map.instance
+          });
+        });
+      });
       // Make sure the maps API has loaded
       if (GoogleMaps.loaded()) {
         // Map initialization options
         return {
           center: new google.maps.LatLng(10.776077000,106.680528000),
-          
+
           zoom: 12
         };
       }
